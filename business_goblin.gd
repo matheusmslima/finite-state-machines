bf370758ca18db66fe51160cd5a7f767e9a59103
matheusmslima <matheusmslima@gmail.com>
2024-10-10 20:52:24 -0300

    finished up the tutoria - learned how powerful state machines are in game development

--- business_goblin.gd
+++ business_goblin.gd
@@ -5,6 +5,13 @@ extends CharacterBody2D
 @export var acceleration = 40.0
 
 @onready var ray_cast_2d = $RayCast2D
+@onready var finite_state_machine = $FiniteStateMachine
+@onready var enemy_wander_state = $FiniteStateMachine/EnemyWanderState
+@onready var enemy_chase_state = $FiniteStateMachine/EnemyChaseState
+
+func _ready():
+	enemy_wander_state.found_player.connect(finite_state_machine.change_state.bind(enemy_chase_state))
+	enemy_chase_state.lost_player.connect(finite_state_machine.change_state.bind(enemy_wander_state))
 
 func _physics_process(delta):
 	ray_cast_2d.target_position = get_local_mouse_position()
